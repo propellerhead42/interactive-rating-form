5d9c8a8b632685c3b7b562696cb156c36c4b4773
Patrik <84462634+propellerhead42@users.noreply.github.com>
2022-04-30 12:34:24 +0200

    finished rating card

--- src/components/RatingCard.vue
+++ src/components/RatingCard.vue
@@ -1,47 +1,31 @@
 <template>
   <section class="card rating-card">
       <div class="logo-container">
-
         <img class="circle-bgn" src="../assets/images/icon-star.svg" alt="">
-
-
       </div>
-      <h3 class="heading-3">How did we do?</h3>
-      <p class="main-p">
-          Please let us know how we did with your support request. All feddback is appreciated to help us improve our offering!
-      </p>
+      <article class="card-description">
+            <h3 class="description-heading">How did we do?</h3>
+            <p class="description-para">
+                Please let us know how we did with your support request. All feddback is appreciated to help us improve our offering!
+            </p>
+        </article>
       <form @submit.prevent="handleSubmit" class="card-rating-form">
-          <section class="checkbox-container">
-              <div class="checkbox-wrapper">
-                    <label class="main-p circle-bgn" >1
-                    <input type="radio" value="1" name="ratingNumber" v-model="ratingNumber">
-                    </label>
-              </div>
-              <div class="checkbox-wrapper">
-                    <label class="main-p circle-bgn" >
-                    <input type="radio" value="2" name="ratingNumber" v-model="ratingNumber">
-                    </label>
-              </div>
-              <div class="checkbox-wrapper">
-                    <label class="main-p circle-bgn" >3
-                    <input type="radio" value="3" name="ratingNumber" v-model="ratingNumber">
-                    </label>
-              </div>
-              <div class="checkbox-wrapper">
-                    <label class="main-p circle-bgn" >4
-                    <input type="radio" value="4" name="ratingNumber" v-model="ratingNumber">
-                    </label>
-              </div>
-              <div class="checkbox-wrapper">
-                    <label class="main-p circle-bgn" >5
-                    <input type="radio" value="5" name="ratingNumber" v-model="ratingNumber">
-                    </label>
-              </div>
-              <div class="errorModal" v-if="errorMsg">
-                  <p>{{errorMsg}}</p>
+            <section class="radio-input-container">
+                <input type="radio" value="1" name="ratingNumber" v-model="ratingNumber">
+
+                <input type="radio" value="2" name="ratingNumber" v-model="ratingNumber">
+                    
+                <input type="radio" value="3" name="ratingNumber" v-model="ratingNumber">
+                    
+                <input type="radio" value="4" name="ratingNumber" v-model="ratingNumber">
+                    
+                <input type="radio" value="5" name="ratingNumber" v-model="ratingNumber">
+
+                <div class="errorModal" v-if="errorMsg">
+                    <p>{{errorMsg}}</p>
                 </div>
-          </section>
-        <button type="submit">SUBMIT</button>
+            </section>
+            <button type="submit">SUBMIT</button>
       </form>
   </section>
 </template>
@@ -79,25 +63,46 @@ export default {
     border-radius: 2rem;
     display: flex;
     flex-direction: column;
-    gap: 2rem;
+    gap: 1.5rem;
 }
 
-.heading-3 {
+.description-heading {
     color: $white;
+    font-size: 2rem;
+    padding: 1rem 0;
 }
 
-.main-p {
+.description-para {
     color: $light-grey;
+    line-height: 1.5rem;
 }
 
 .card-rating-form {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 1.25rem;
+
+    button {
+        width: 100%;
+        margin: 0 auto;
+        padding: 0.6rem 0;
+        border-radius: 3rem;
+        outline: none;
+        border: none;
+        font-weight: 700;
+        color: $white;
+        background: $orange;
+        cursor: pointer;
+        letter-spacing: 2px;
+
+        &:hover {
+            color: $orange;
+            background: $white;
+        }
+    }
 }
 
 .logo-container {
-    padding: 1rem 0;
     display: flex;
     align-items: center;
 
@@ -108,71 +113,72 @@ export default {
     }
 }
 
-.checkbox-container {
+.radio-input-container {
     position: relative;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
-    padding-bottom: 2rem;
-}
+    padding: 0.5rem 0;
 
-.checkbox-wrapper {
-    position: relative;
-    .circle-bgn {
+    input{
+        appearance: none;
+        width: 2.8rem;
+        height: 2.8rem;
         display: block;
-        width: 3rem;
-        height: 3rem;
+        margin: auto 0;
+        background: $round-bgn-form;
         border-radius: 50%;
         display: flex;
         justify-content: center;
         align-items: center;
-    }
-
-    label {
-        color: $white;
-        z-index: 999;
-
-        &:hover {
-        }
-    }
-
-    input {
-        position: absolute;
-        appearance: none;
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
         cursor: pointer;
-        background-color: $round-bgn-form;
 
-        &::after {
-            position: relative;
-            content: " ";
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            height: 100%;
+        &::after{
+            color: $light-grey;
         }
-        
+
+        &:nth-child(1)::after{
+            content: "1"
+        }
+        &:nth-child(2)::after{
+            content: "2"
+        }
+        &:nth-child(3)::after{
+            content: "3"
+        }
+        &:nth-child(4)::after{
+            content: "4"
+        }
+        &:nth-child(5)::after{
+            content: "5"
+        }
+
         &:hover {
             background-color: $light-grey;
+            color: $white;
+
+            &::after{
+                color: $white;
+            }
         }
 
-        &:checked{
+        &:checked {
             background-color: $orange;
+            
+            &::after{
+                color: $white;
+            }
         }
-
     }
 }
 
+
 .errorModal {
     position: absolute;
-    bottom: 0;
+    top: -1.25rem;
     left: 0;
     right: 0;
-    background: rgb(164, 19, 19);
-    color: $white;
+    color: $red;
     font-weight: 700;
     display: flex;
     justify-content: center;
